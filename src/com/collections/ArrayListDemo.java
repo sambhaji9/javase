e42e5a4091c1a99cf237e41f031279cad9844620
Sambhaji Mane (CC313640) <sambhajim@nedbank.co.za>
2020-08-03 21:12:34 +0200

    Added arraylist demo class

--- src/com/collections/ArrayListDemo.java
+++ src/com/collections/ArrayListDemo.java
@@ -21,9 +21,27 @@ public class ArrayListDemo {
         // display the arraylist
         System.out.println("Contents of arrayList: " + arrayList);
 
+        System.out.println("\nMethods for arrayList");
         // remove elements from the arraylist
         arrayList.remove("2");
         System.out.println("Contents of arrayList after deletion: " + arrayList);
         System.out.println("Size of arrayList after deletions: " + arrayList.size());
+
+        // checkif some element is in collection
+        System.out.println("\ncheck if some element is in collection");
+        System.out.println("t is in collection arrayList: " + arrayList.contains("t"));
+        System.out.println("100 is in collection arrayList: " + arrayList.contains("100"));
+
+        // checkif all the members are in another collection
+        System.out.println("\ncheck if all the members are in another collection");
+        ArrayList<String> arrayList3 = new ArrayList<String>();
+        arrayList3.add("S");
+        arrayList3.add("a");
+        arrayList3.add("t");
+        System.out.println("Sat is in collection arrayList: " + arrayList.containsAll(arrayList3));
+
+        ArrayList<String> arrayList2 = new ArrayList<String>();
+        arrayList2.addAll(0, arrayList);
+        System.out.println(arrayList2);
     }
 }
