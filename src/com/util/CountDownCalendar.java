3adf6a4f2bce967a0341936829582bd4a9fb8130
Sambhaji Mane (CC313640) <sambhajim@nedbank.co.za>
2020-08-10 13:27:38 +0200

    Added the comments

--- src/com/util/CountDownCalendar.java
+++ src/com/util/CountDownCalendar.java
@@ -5,14 +5,18 @@ import java.util.GregorianCalendar;
 // demonstrate a countdown calendar
 public class CountDownCalendar {
     public static void main(String[] args) {
+
+        // get the current date
         GregorianCalendar gregorianCalendarNow = new GregorianCalendar();
         gregorianCalendarNow.set(2020,7, 10);
         int now = gregorianCalendarNow.get(GregorianCalendar.DAY_OF_YEAR);
 
+        //get the date in future
         GregorianCalendar gregorianCalendarDiwali = new GregorianCalendar();
         gregorianCalendarDiwali.set(2020, 10, 12);
         int diwali = gregorianCalendarDiwali.get(GregorianCalendar.DAY_OF_YEAR);
 
+        // Get the difference between the dates
         System.out.println("Days till Diwali vacation: " + (diwali - now));
     }
 }
