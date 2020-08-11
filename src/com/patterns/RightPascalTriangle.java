5bf32ea2b240235e2bca3ec24f1428fc9edf0063
Sambhaji Mane (CC313640) <sambhajim@nedbank.co.za>
2020-08-11 18:24:49 +0200

    Added some more patterns for SummedNumber and RightPascaltriangle

---
+++ src/com/patterns/RightPascalTriangle.java
@@ -0,0 +1,30 @@
+package com.patterns;
+
+/**
+ * Demonstrate the right pascal triangle
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ * ****
+ * ***
+ * **
+ * *
+ */
+public class RightPascalTriangle {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i < 5) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" * ");
+                }
+            } else {
+                for (int j = 0; j < 10 - i; j++) {
+                    System.out.print(" * ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
