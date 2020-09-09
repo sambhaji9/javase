20826fe50299a105d6c7d079683d4dcd147cea5c
Sambhaji Mane (CC313640) <sambhajim@nedbank.co.za>
2020-09-09 19:33:12 +0200

    Added demos for some more string handling demos

---
+++ src/com/string_handling/StringConversions.java
@@ -0,0 +1,10 @@
+package com.string_handling;
+
+public class StringConversions {
+    public static void main(String[] args) {
+        int num = 19648;
+        System.out.println(num + " in binary: " + Integer.toBinaryString(num));
+        System.out.println(num + " in octal: " + Integer.toOctalString(num));
+        System.out.println(num + " in hexadecimal: " + Integer.toHexString(num));
+    }
+}
